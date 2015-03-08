64a97a54d74103036866d073b63d4d12f6f1ca8f
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-08 23:11:57 +0900

    Fix the authorization for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate_admin_user!
-    return authenticate_admin_user! unless current_user
+    return authenticate_user! unless current_user
     return if current_user.admin?
     redirect_to root_path, alert: I18n.t('devise.failure.unauthenticated')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'books#index'
+
   ActiveAdmin.routes(self)
   devise_for :users
   resources :books
