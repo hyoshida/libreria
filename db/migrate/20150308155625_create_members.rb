69c352134bd0d82bfb37c3295def7d6bc62e47c8
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-10 13:57:46 +0900

    Fix the migration of members

--- db/migrate/20150308155625_create_members.rb
+++ db/migrate/20150308155625_create_members.rb
@@ -1,10 +1,11 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-      t.references :organization, null: false, index: { unique: [:organization, :user] }
+      t.references :organization, null: false
       t.references :user, null: false
       t.string :role, null: false
       t.timestamps null: false
+      t.index [:organization_id, :user_id], unique: true
     end
   end
 end
