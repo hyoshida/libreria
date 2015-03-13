d2e8394fc04ba744246d35979eac2bb07373ae53
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-13 12:23:33 +0900

    Add the name filed to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
+#  name                   :string           not null
 #  role                   :string           not null
 #  email                  :string           not null
 #  encrypted_password     :string           not null
@@ -36,7 +37,10 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :namespace
 
   validates :namespace, presence: true
+  validates :name, presence: true, length: { maximum: 255 }
   validates :role, presence: true, length: { maximum: 255 }
+
+  before_validation :copy_name_from_namespace, on: :create
 
   delegate :path, to: :namespace
 
@@ -53,11 +57,17 @@ class User < ActiveRecord::Base
     path
   end
 
-  def name
+  def as_json(options)
+    { id: id, text: email }
+  end
+
+  def username
     namespace.path
   end
 
-  def as_json(options)
-    { id: id, text: email }
+  private
+
+  def copy_name_from_namespace
+    self.name = username
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  subject { create(:user) }
+
   should have_one(:namespace).dependent(:destroy)
   should have_many(:members).dependent(:destroy)
   should have_many(:organizations).through(:members)
@@ -10,6 +12,8 @@ class UserTest < ActiveSupport::TestCase
   should accept_nested_attributes_for(:namespace)
 
   should validate_presence_of(:namespace)
+  should validate_presence_of(:name)
   should validate_presence_of(:role)
+  should validate_length_of(:name).is_at_most(255)
   should validate_length_of(:role).is_at_most(255)
 end
