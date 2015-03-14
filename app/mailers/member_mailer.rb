4233477cb723cb8b6ec967608996cc0c51370e80
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-14 23:56:21 +0900

    Replace text to I18n for mailer

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -2,12 +2,12 @@ class MemberMailer < ApplicationMailer
   def requests(organization, member)
     @organization = organization
     @member = member
-    mail to: @organization.email, subject: 'Requests to join your organization!'
+    mail to: @organization.email, subject: t('member_mailer.requests.subject')
   end
 
   def accept(organization, member)
     @organization = organization
     @member = member
-    mail to: @member.email, subject: 'Accept your request!'
+    mail to: @member.email, subject: t('member_mailer.accept.subject')
   end
 end
