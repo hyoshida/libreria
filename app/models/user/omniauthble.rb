98dd2e0342f7269e5e05c68187339747727c523c
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-19 18:44:40 +0900

    Refactor the code

--- app/models/user/omniauthble.rb
+++ app/models/user/omniauthble.rb
@@ -4,16 +4,11 @@ class User < ActiveRecord::Base
 
     module ClassMethods
       def from_omniauth(auth)
-        uid_column = "#{auth.provider}_uid"
-        user = where(uid_column => auth.uid).first
-        user ||= where(email: auth.info.email).first_or_initialize
-        user.attributes = {
-          uid_column => auth.uid,
-          name: auth.info.name,
-          email: auth.info.email,
-          password: Devise.friendly_token[0, 20],
-          namespace_attributes: { path: auth.info.email.split('@').first }
-        }
+        user = find_by_auth(auth)
+        user ||= find_by(email: auth['info']['email'])
+        user ||= new
+        user.attributes = attributes_from_auth(auth)
+        user.password = Devise.friendly_token[0, 20] if user.new_record?
         user
       end
 
@@ -21,16 +16,15 @@ class User < ActiveRecord::Base
       def new_with_session(params, session)
         super.tap do |user|
           auth = find_provider_and_auth_from_session(session)
-          if auth
-            user.send("#{auth['provider']}_uid=", auth['uid'])
-            user.name = auth['info']['name'] if user.name.blank?
-            user.email = auth['info']['email'] if user.email.blank?
-            user.build_namespace(path: auth['info']['email'].split('@').first) unless user.namespace
-          end
+          user.attributes = attributes_from_auth(auth) if auth
         end
       end
 
       private
+
+      def find_by_auth(auth)
+        find_by("#{auth['provider']}_uid" => auth['uid']) if auth
+      end
 
       def find_provider_and_auth_from_session(session)
         omniauth_providers.each do |provider|
@@ -38,6 +32,20 @@ class User < ActiveRecord::Base
           return auth if auth
         end
         nil
+      end
+
+      def username_from_email(email)
+        return if email.blank?
+        email.split('@').first
+      end
+
+      def attributes_from_auth(auth)
+        {
+          :"#{auth['provider']}_uid" => auth['uid'],
+          name: auth['info']['name'],
+          email: auth['info']['email'],
+          namespace_attributes: { path: username_from_email(auth['info']['email']) }
+        }
       end
     end
   end
