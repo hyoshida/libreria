7d34b3f60e528495ca9f49836debc333386a431a
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-20 10:51:30 +0900

    Sign up directly by OmniAuth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,9 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
-      if @user.persisted?
+      if @user.persisted? || @user.save
         sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
